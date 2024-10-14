7554ae1b2d0def965e4ed0a2e299d0d3ca2dd462
kshipraDewat <kshipradewat66@gmail.com>
2024-10-14 18:18:10 +0530

    cart api

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ import userRouter from './routes/userRoute.js';
 
 import 'dotenv/config'
 import foodRouter from './routes/foodRoute.js';
+import cartRouter from './routes/cartRoute.js';
 
 
 const app = express()
@@ -21,6 +22,7 @@ connectDB()
 app.use("/api/user", userRouter)
 app.use("/api/food", foodRouter)
 app.use("/images",express.static('uploads'))
+app.use("/api/cart", cartRouter)
 
 app.get("/", (req, res) => {
     res.send("API Working")
