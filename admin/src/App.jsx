5a55f4f6fabac1732974a52ed35c2077c13f3a5a
kshipraDewat <kshipradewat66@gmail.com>
2024-10-16 03:19:12 +0530

    Admin panel routes setup

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,13 +1,25 @@
 import Navbar from "./component/Navbar"
 import Sidebar from "./component/Sidebar"
+import { Route, Routes } from 'react-router-dom'
+import Add from "./pages/Add"
+import List from "./pages/List"
+import Orders from "./pages/Orders"
 
 function App() {
-  
+
   return (
-   <div  className=" ">
-    <Navbar/>
-    <Sidebar/>
-   </div>
+    <div className=" ">
+      <Navbar />
+      <div className="flex ">
+        <Sidebar />
+        <Routes>
+          <Route path="/add" element={<Add />} />
+          <Route path="/list" element={<List />} />
+          <Route path="/orders" element={<Orders />} />
+        </Routes>
+      </div>
+
+    </div>
   )
 }
 
