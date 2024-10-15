ba07717266b863ff7e32527e3769fd242320cfc0
kshipraDewat <kshipradewat66@gmail.com>
2024-10-15 19:57:05 +0530

    added .env

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const  connectDB = async () =>{
 
-    await mongoose.connect('mongodb+srv://kshipradewat66:kshipradewat66@cluster0.bp9pi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/test')
+    await mongoose.connect(process.env.DB_URL)
     .then(()=>console.log("DB Connected"));
    
 }
--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -4,7 +4,7 @@ import axios from 'axios'
 
 const StoreContextProvider = (props) => {
 
-    const url = "http://localhost:4000"
+    const url = import.meta.env.VITE_BACKEND_URL
     const [food_list, setFoodList] = useState([]);
     const [cartItems, setCartItems] = useState({});
     const [token, setToken] = useState("")
